4139a706cedcda115188c0d5a4441f37102c4636
MrMohammadY <m.yazdani000@gmail.com>
2020-10-16 11:41:12 +0330

    remove decoratir and edditing for usd_sell

--- config.py
+++ config.py
@@ -2,24 +2,23 @@
 from data import url
 
 
-items = {"1": "sekkeh", "2": "usd_buy"}
+items = {"1": "sekkeh", "2": "usd_buy", "3": "usd_sell"}
 
 
 def get_choice_item():
-    choice_item = input(
+    choice_items = input(
         "for rate gold coin(sekkeh) number 1 \n"
-        "for rate dollor(USD) number 2\n"
-        "Please insert date desired (1 or 2 ) : ").strip()
+        "for rate dollor_buy(USD) number 2\n"
+        "for rate dollor_sell(USD) number 3\n"
+        "Please insert date desired (1 or 2 or 3) : ").strip()
 
-    if choice_item not in ('1', '2'):
+    if choice_items not in ('1', '2', '3'):
         return get_choice_item()
 
-    return choice_item
+    return choice_items
 
 
 def make_url(item):
-    print(f'{url}&item={item}')
     return f'{url}&item={item}'
 
 
-make_url(items[get_choice_item()])
--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 import requests
 import json
-from config import make_url, get_choice_item, items
+from config import make_url, items, get_choice_item
 
 
-def get_api(item):
-    url = make_url(items[item])
-    response = requests.get(url)
+def get_api(address):
+    response = requests.get(address)
     return json.loads(response.text)
 
 
-def show_rate(item):
-    rate = get_api(item)
-    return rate[items[item]]['value'], rate[items[item]]['date']
+def show_rate(address, choice_item):
+    rate = get_api(address)
+    return rate[items[choice_item]]['value'], rate[items[choice_item]]['date']
+
+
+def run():
+    choice_item = get_choice_item()
+    url = make_url(items[choice_item])
+    final_rate = show_rate(url, choice_item)
+
+    print('*' * 30)
+    print(f'item : {items[choice_item]}')
+    print(f'ِDate and time : {final_rate[1]}')
+    print(f'final rate : {final_rate[0]}')
+
+    play_again = input('if you want try again please press (y)'
+                       ' and for exit press (q): ')
+
+    if play_again == 'y':
+        return run()
+
+    print('see you')
 
 
 if __name__ == '__main__':
-    choice_items = get_choice_item()
-    final_rate = show_rate(choice_items)
-    print('*' * 30)
-    print(f'item : {items[choice_items]}')
-    print(f'ِDate and time : {final_rate[1]}')
-    print(f'final rate : {final_rate[0]}')
+    run()
